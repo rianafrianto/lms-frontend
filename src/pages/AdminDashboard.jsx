b0971f1366ffaa808e04986f9ff7daf55e3097d8
rianafrianto <rianafrianto17@gmail.com>
2024-11-26 11:31:47 +0700

    adjust headers

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -138,7 +138,7 @@ const AdminDashboard = () => {
         {/* Gradient header */}
         <div className="bg-gradient-to-r from-indigo-500 to-blue-600 text-white p-6 shadow-lg rounded-lg mx-auto mb-6 max-w-full">
           <h1 className="text-3xl font-bold text-center sm:text-4xl">Admin Dashboard</h1>
-          <div className="w-16 h-1 bg-white mx-auto mt-2 mb-4"></div>
+          <div className="w-16 h-1 bg-white mx-auto mt-6 mb-4"></div>
           <p className="text-center text-lg sm:text-xl">
             Manage the courses submitted by users with ease.
           </p>
