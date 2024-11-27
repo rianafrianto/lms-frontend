0415657874eab92bc6a2e574c5c95befc83e1519
rianafrianto <rianafrianto17@gmail.com>
2024-11-27 16:39:13 +0700

    fixing button hover

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -18,7 +18,7 @@ const LandingPage = () => {
                         Login
                     </button>
                     <button
-                        className="ml-4 bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-green-600 transition"
+                        className="ml-4 bg-blue-600 text-white px-4 py-2 rounded-lg shadow hover:bg-blue-700 transition"
                         onClick={() => navigate('/register')}
                     >
                         Register
